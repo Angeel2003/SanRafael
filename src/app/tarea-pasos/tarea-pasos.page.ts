fa2cc41c5e201ea1f63242009fbcc9957351fe50
Ane <e.aneanta15@go.ugr.es>
2024-11-03 14:14:30 +0100

    Firebase añadido

--- src/app/tarea-pasos/tarea-pasos.page.ts
+++ src/app/tarea-pasos/tarea-pasos.page.ts
@@ -9,6 +9,21 @@ import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonIcon, IonButton, 
         IonInput, IonItem, IonLabel, IonTabs, IonTabBar, IonTabButton, IonList} from '@ionic/angular/standalone';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNS4uHdWMqQ6huVkqV4FhYtQi_9Jozn88",
+  authDomain: "indepapp-sanrafael.firebaseapp.com",
+  projectId: "indepapp-sanrafael",
+  storageBucket: "indepapp-sanrafael.firebasestorage.app",
+  messagingSenderId: "65579135683",
+  appId: "1:65579135683:web:7dcb776bfbec36c67fe876",
+  measurementId: "G-2K9V8MCKWE"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @Component({
   selector: 'app-tarea-pasos',
