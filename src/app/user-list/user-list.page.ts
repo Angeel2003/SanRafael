35299180080e09446438d56a97409b9d7c4d9ef7
Ane <e.aneanta15@go.ugr.es>
2024-11-03 16:23:04 +0100

    Firebase bien integrado

--- src/app/user-list/user-list.page.ts
+++ src/app/user-list/user-list.page.ts
@@ -1,24 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { FirebaseService } from '../services/firebase.service';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonIcon, IonButtons, IonButton, IonBackButton, 
         IonCard, IonCardHeader, IonCardTitle} from '@ionic/angular/standalone';
-import { NgFor } from '@angular/common';
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBNS4uHdWMqQ6huVkqV4FhYtQi_9Jozn88",
-  authDomain: "indepapp-sanrafael.firebaseapp.com",
-  projectId: "indepapp-sanrafael",
-  storageBucket: "indepapp-sanrafael.firebasestorage.app",
-  messagingSenderId: "65579135683",
-  appId: "1:65579135683:web:7dcb776bfbec36c67fe876",
-  measurementId: "G-2K9V8MCKWE"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 @Component({
   selector: 'app-user-list',
@@ -36,7 +21,7 @@ export class UserListPage implements OnInit {
     // Agrega más usuarios según sea necesario
   ];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private firebaseService: FirebaseService) { }
 
   ngOnInit() {
   }
--- src/app/user-list/user-login/user-login.page.ts
+++ src/app/user-list/user-login/user-login.page.ts
@@ -2,25 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { lockClosedOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { NgIf } from '@angular/common';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonIcon, IonButtons, IonButton, IonBackButton, 
   IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
-import { NgIf } from '@angular/common';
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBNS4uHdWMqQ6huVkqV4FhYtQi_9Jozn88",
-  authDomain: "indepapp-sanrafael.firebaseapp.com",
-  projectId: "indepapp-sanrafael",
-  storageBucket: "indepapp-sanrafael.firebasestorage.app",
-  messagingSenderId: "65579135683",
-  appId: "1:65579135683:web:7dcb776bfbec36c67fe876",
-  measurementId: "G-2K9V8MCKWE"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-user-login',
@@ -36,7 +22,7 @@ export class UserLoginPage implements OnInit {
   isPIN: boolean = false;
   isPictograma: boolean = false;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private firebaseService: FirebaseService) {
     addIcons({
       lockClosedOutline
     })
