ecabd62700246d616194c9e7375b8eb4e5cbe23f
Angeel2003 <127200757+Angeel2003@users.noreply.github.com>
2024-11-08 12:08:45 +0100

    Update tarea-material.page.ts

--- src/app/tarea-material/tarea-material.page.ts
+++ src/app/tarea-material/tarea-material.page.ts
@@ -1,20 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-import { addIcons } from 'ionicons';
-import { arrowBackOutline, personCircleOutline, addOutline } from 'ionicons/icons';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonButton, IonCol, IonIcon, IonItem, IonInput, IonLabel, IonList, IonFooter } from '@ionic/angular/standalone';
-
-import { FirebaseService } from '../services/firebase.service';
-
-import { Router } from '@angular/router';
-=======
 import { saveOutline } from 'ionicons/icons';
 import { addOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonButton, IonButtons, IonCol, IonIcon, IonItem, IonInput, IonLabel, IonList, IonBackButton } from '@ionic/angular/standalone';
->>>>>>> master
 
 interface MaterialItem {
   material: string;
@@ -28,26 +18,13 @@ interface MaterialItem {
   templateUrl: './tarea-material.page.html',
   styleUrls: ['./tarea-material.page.scss'],
   standalone: true,
-<<<<<<< HEAD
-  imports: [IonFooter, IonList, IonLabel, IonInput, IonItem, IonIcon, IonCol, IonButton, IonRow, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
-=======
   imports: [IonList, IonLabel, IonInput, IonItem, IonIcon, IonCol, IonButton, IonButtons, IonRow, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, CommonModule, FormsModule]
->>>>>>> master
 })
 
 export class TareaMaterialPage implements OnInit {
   taskName: string = '';
   imgTarea: string = '';
   items: MaterialItem[] = [];
-
-<<<<<<< HEAD
-  constructor(private firebaseService: FirebaseService, private router: Router) {
-    addIcons({
-      arrowBackOutline,
-      personCircleOutline,
-      addOutline
-    });
-=======
   
   constructor() {
     addIcons({
@@ -57,7 +34,6 @@ export class TareaMaterialPage implements OnInit {
   }
 
   ngOnInit() {
->>>>>>> master
   }
 
   goBackToAdmin(){
