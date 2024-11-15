cca63be2f6af41ac15f8b10c135c96af49686990
Léo Gonnet <leogonnet14@gmail.com>
2024-11-15 15:02:24 +0100

    mini fix comments

--- src/app/crear-tarea-comanda/crear-tarea-comanda.page.spec.ts
+++ src/app/crear-tarea-comanda/crear-tarea-comanda.page.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CrearTareaComandaPage } from './crear-tarea-comanda.page';
 
-describe('TareaMaterialPage', () => {
+describe('CrearTareaComandalPage', () => {
   let component: CrearTareaComandaPage;
   let fixture: ComponentFixture<CrearTareaComandaPage>;
 
